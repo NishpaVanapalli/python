6138ef2409d5120ec910e07ab8f539e356261c36
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-09-30 12:26:42 +0530

    Add files via upload

---
+++ loops examples.py
@@ -0,0 +1,56 @@
+##eligible for campus dirve or not##
+##p=float(input('enter your percentage'))
+##bl=int(input('enter number of backlogs'))
+##if p>=60 and bl==0:
+##   print("you are eligible for the campus drive")
+##else:
+##  print("sorry! you are not eligibe")
+
+
+##less than or greater than##
+##a=int(input("enter a number"))
+##b=int(input("enter a number"))
+##if a < b:
+##  print("a is less than b", a, b)
+##else:
+##  print("a is greater than b", a, b)
+
+
+##program to declare  the grade obtained using marks##
+##m= int(input('enter your marks'))
+##if m>=80:
+##  print("A grade")
+##elif m>=60 and m<80:
+##  print("B grade")
+##elif m>=40 and m<60:
+##  print("C grade")
+##else:
+##  print("better luck next time")
+
+
+##to print the multiple of even/odd numbers##
+##s= int(input('enter a start number '))
+##e= int(input('enter a end number '))
+##b= int(input('enter a multiplication '))
+##for n in range(s,e+1):
+##    if n%2==1:
+##        print(n,'*',b,'=',n*b)
+
+
+##login using for and else##
+##id= 'thub@gmail.com'
+##pw= '12345'
+##
+##for i in range(4):
+##    uid= str(input('enter your mail id '))
+##    upw= str(input('enter your password '))
+##    if id==uid and pw==upw:
+##        print('login successful')
+##        break
+##    else:
+##        print('wrong credentials')
+##
+##else:
+##    print('account blocked')
+##
+##print('all are done')
