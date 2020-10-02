7a73d673ff53421fc73836ee52a5c0ddec2e88fd
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-10-02 12:15:48 +0530

    Add files via upload

---
+++ loops patterns.py
@@ -0,0 +1,106 @@
+##pattern printing n*n using for loop##
+##n=int(input('enter a number '))
+##for i in range(1,n+1):
+##    for j in range(1,n+1):
+##        print('*',end=' ')
+##    print( )
+
+
+
+
+
+##pattern printing n*n using while loop##
+##n=int(input('enter a number '))
+##i=1
+##while i<=n:
+##    j=1
+##    while j<=n:
+##        print('*',end=' ')
+##        j+=1
+##    print( )
+##    i+=1
+    
+   
+
+
+
+##pattern printing numbers 1*n##
+##n= int(input('enter a number '))
+##for i in range(1,n+1):        #row
+##    for j in range(1,n+1):       #column
+##        print(i,end=' ')
+##    print( )
+
+
+
+
+
+##pattern printing i*n using for loop##
+##n= int(input('enter a number '))
+##for i in range(1,n+1):
+##    for j in range(1,i+1):
+##        print('*',end=' ')
+##    print( )
+
+
+
+
+
+##pattern printing 1*n using while loop##
+##n=int(input('enter a number '))
+##i=1
+##while i<=n:
+##    j=1
+##    while j<=i:
+##        print('*',end=' ')
+##        j+=1
+##    print( )
+##    i+=1
+
+
+
+
+
+##pattern printing numbers 1-n##
+##n= int(input('enter a number '))    
+##for i in range(1,n+1):
+##    for j in range(1,i+1):
+##        print(j,end=' ')
+##    print( )
+
+
+
+
+
+##pattern printing n*1##
+##n=int(input('enter a number '))
+##for i in range(1,n+1):
+##    for j in range(n,i-1,-1):
+##        print('*',end=' ')
+##    print( )
+
+
+
+
+
+##pattern printing design 1*n triangle##
+##n=int(input('enter a number '))
+##for i in range(1,n+1):
+##    for k in range(n,i,-1):
+##        print(' ',end=' ')
+##    for j in range(1,i+1):
+##        print(' * ',end=' ')
+##    print( )
+
+
+
+
+
+##pattern printing design n*n square##
+##n=int(input('enter a number '))
+##for i in range(1,n+1):
+##    for k in range(n,i,-1):
+##        print(' ',end=' ')
+##    for j in range(1,n+1):
+##        print('*',end=' ')
+##    print( )
