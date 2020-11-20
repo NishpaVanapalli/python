128c85986d5836ef7a7df2466b6c3877f2b15a11
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-11-20 18:55:48 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,46 @@
+##secret_number = 777
+##
+##print(
+##"""
+##+================================+
+##| Welcome to my game, muggle!    |
+##| Enter an integer number        |
+##| and guess what number I've     |
+##| picked for you.                |
+##| So, what is the secret number? |
+##+================================+
+##""")
+##
+##user_number = int(input("Enter the number: "))
+##
+##while user_number != secret_number:
+##    print("Ha ha! You're stuck in my loop!")
+##    user_number = int(input("Enter the number again: "))
+##print(secret_number, "Well done, muggle! You are free now.")
+
+
+
+##userWord = input("Enter your word: ")
+##userWord = userWord.upper()
+##
+##for letter in userWord:
+##    if letter == "A":
+##        continue
+##    elif letter == "E":
+##        continue
+##    elif letter == "I":
+##        continue
+##    elif letter == "O":
+##        continue
+##    elif letter == "U":
+##        continue
+##    else:
+##        print(letter)
+
+
+
+##while True:
+##    word = input("You're stuck in an infinite loop! Enter the secret word to leave the loop: ")
+##    if word == "chupacabra":
+##        break
+##print("You've successfully left the loop!")
