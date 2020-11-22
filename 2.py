1e0f4d784e16038e758e4a1db1c0da1ebce6ed27
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-11-22 11:14:22 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,49 @@
+##wordWithoutVowels = " "
+##
+##userWord = input("Enter your word: ")
+##userWord = userWord.upper()
+##
+##for letter in userWord:
+##    if letter == "A":
+##        continue
+##    elif letter == "E":
+##        continue
+##    elif letter == "I":
+##        continue
+##    elif letter == "O":
+##        continue
+##    elif letter == "U":
+##        continue
+##    else:
+##        wordWithoutVowels += letter
+##		
+##print(wordWithoutVowels)
+
+
+
+##i = 1
+##while i < 5:
+##    print(i)
+##    i += 1
+##else:
+##    print("else:", i)
+
+
+
+##for i in range(5):
+##    print(i)
+##else:
+##    print("else:", i)
+
+
+
+##blocks = int(input("Enter the number of blocks: "))
+##
+##height = 0
+##inlayer = 1
+##while inlayer <= blocks:
+##    height += 1
+##    blocks -= inlayer
+##    inlayer += 1
+##
+##print("The height of the pyramid:", height)
