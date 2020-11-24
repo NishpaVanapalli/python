98ac0e999bf4a87df3fa647e3adede4525c1ca18
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-11-24 20:05:59 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,46 @@
+##i=2
+##while i>=0:
+##    print("*")
+##    i-=2
+
+
+##for i in range(-1,1):
+##    print("#")
+
+
+##z=10
+##y=0
+##x=z>y or z==y
+##print(x)
+
+
+##lst=[3,1,-1]
+##lst[-1]=lst[-2]
+##print(lst)
+
+
+
+##nums=[ ]
+##vals=nums
+##vals.append(1)
+##print(nums)
+##print(vals)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
