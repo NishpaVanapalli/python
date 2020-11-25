94464968aaaf4c9b87b06c81ff8447023de649a5
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-11-25 18:40:28 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,36 @@
+##l=[0 for i in range(1,3)]
+##print(l)
+
+
+##lst=[0,1,2,3]
+##x=1
+##for elem in lst:
+##    x*=elem
+##print(x)
+
+
+##vals=[0,1,2]
+##vals[0],vals[1]=vals[1],vals[2]
+##print(vals[2])
+
+
+
+
+##i=0
+##while i<=3:
+##    i+=2
+##    print("*")
+
+
+##for i in range(1):
+##    print("#")
+##else:
+##    print("#")
+
+
+
+##lst1=[1,2,3]
+##lst2=[ ]
+##for v in lst1:
+##    lst2.insert(0,v)
+##print(lst2)
