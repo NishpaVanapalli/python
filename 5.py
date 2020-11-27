0c3e3beef13bc73fc184c55b1c01139624982904
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-11-27 20:46:42 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,43 @@
+##i=0
+##while i<=5:
+##    i+=1
+##    if i%2==0:
+##        break
+##    print("*")
+
+
+
+##a=1
+##b=0
+##c=a&b
+##d=a|b
+##e=a^b
+##print(c+d+e)
+
+
+
+##var=0
+##while var<6:
+##    var+=1
+##    if var%2==0:
+##         continue
+##    print("#")
+
+
+
+##nums=[1,2,3]
+##vals=nums
+##del vals[1:2]
+##print(vals)
+##print(nums)
+
+
+##lst=[1,2,3,4]
+##print(lst[-3:-2])
+
+
+
+##lst=[1,2,3]
+##for v in range(len(lst)):
+##    lst.insert(1,lst[v])
+##print(lst)
