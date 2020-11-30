791a480b1f69566fafde04d97a056e72e232b48c
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-11-30 20:25:41 +0530

    Add files via upload

---
+++ prac.py
@@ -0,0 +1,12 @@
+##from func import add
+##a= int(input('enter a number '))
+##b= int(input('enter a number '))
+##print(mul(a,b))
+
+
+
+##import func
+##n= int(input('enter a number '))
+##print(func.prime(n))
+
+
