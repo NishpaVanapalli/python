dbb3a2cbb4006dbe5d1aabd020b057bffe9e9aa9
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-12-01 18:13:15 +0530

    Add files via upload

---
+++ prac1.py
@@ -0,0 +1,268 @@
+##x=input()
+##y=int(input())
+##print(x*y)
+
+
+##x=1
+##y=2
+##x, y, z = x, x, y
+##z, y, z = x, y, z
+##print(x,y,z)
+
+
+##x=int(input())
+##y=int(input())
+##x = x % y
+##x = x % y
+##y = y % x
+##print(y)
+
+
+
+##if False:
+##    print("Nissan")
+##elif True:
+##    print("Ford")
+##elif True:
+##    print("BMW")
+##else:
+##    print("Audi")
+
+
+##x = -5
+##a = 1
+##b = -5
+##if a > 0:
+##    if b < 0: 
+##        x = x + 5 
+##    elif a > 5:
+##        x = x + 4
+##    else:
+##        x = x + 3
+##else:
+##    x = x + 2
+
+
+##for i in range(100):
+##    print(end="")
+##print(i)
+
+
+##x = 0
+##while (x < 100):
+##  x+=2
+##print(x)
+
+
+##if True:
+##    print(101)
+##else:
+##    print(202)
+
+
+##if "cat" == "dog":
+##    print("prrrr")
+##else:
+##    print("ruff")
+
+
+##x = 0
+##for i in range(10):
+##  for j in range(-1, -10, -1):
+##    x += 1
+##print(x)
+
+
+##a=10
+##b=20
+##c=10
+##if ( a and b or c):
+##    if (c or b or a):
+##        if(b and c and not a):
+##            print(a)
+##        else:
+##            print(b)
+##    else:
+##        print(c)
+##else:
+##    print(a,b,c)
+
+
+##a, b = 12, 5
+##if a + b:
+##    print('True')
+##else:
+##  print('False')
+
+
+##a="b"
+##c="d"
+##if a is c:
+##  print('Hi')
+##else:
+##   print(''Hello'')
+
+
+##x = 0
+##a = 5
+##b = 5
+##if a > 0:
+##    if b < 0: 
+##        x = x + 5 
+##    elif a > 5:
+##        x = x + 4
+##    else:
+##        x = x + 3
+##else:
+##    x = x + 2
+##print(x)
+
+
+##i = 0
+##while i < 3:
+##    print(i)
+##    i += 1
+##else:
+##    print(0)
+
+
+##for i in range (5, 0, -2):
+##    print(i)
+
+
+##for i in range(3):
+##    break
+
+
+##x = 123
+##for i in x:
+##    print(i)
+
+
+##n=7
+##c=0
+##while(n):
+##    if(n>5):
+##        c=c+n-1
+##        n=n-1
+##    else:
+##        break
+##else:
+##    print(n,c)
+##    
+##print(c,n)
+
+
+##i = 0
+##while i <= 5 :
+##        i += 1
+##        if i % 2 == 0:
+##            break 
+##        print("*")
+
+
+##i = 0
+##while i <= 3 :
+##        i += 2 
+##        print("*")
+
+
+##var = 1
+##while var < 10:
+##        print("#")
+##        var = var << 1
+
+##n=7
+##c=0
+##while(n):
+##    if(n>5):
+##        c=c+n-1
+##        n=n-1
+##    else:
+##        print(n,c)
+##        break
+
+
+##n=7
+##c=0
+##while(n):
+##    if(n>5):
+##        c=c+n-1
+##        n=n-1
+##        print("if",n,c)
+##        break
+##    else:
+##        print("else",n,c)
+##        break
+##else:
+##    print("loop else",n,c)
+
+##n=0
+##c=10
+##while(n):
+##    if(n>5):
+##        c=c+n-1
+##        n=n-1
+##        print("if",n,c)
+##        break
+##    else:
+##        print("else",n,c)
+##        break
+##else:
+##    print("loop else",n,c)
+
+##i = 1
+##while True:
+##    if i%3 == 0:
+##        break
+##    print(i)
+## 
+##    i += 1
+
+##i = 1
+##while True:
+##    if i%0O7 == 0:
+##        break
+##    print(i,end=" ")
+##    i += 1
+
+##i = 5
+##while True:
+##    if i%0O11 == 0:
+##        break
+##    print(i)
+##    i += 1
+
+##i = 5
+##while True:
+##    if i%0o9 == 0:
+##        break
+##    print(i)
+##    i += 1
+
+##i = 1
+##while True:
+##    if i%2 == 0:
+##        break
+##    print(i)
+##    i += 2
+
+##i = 2
+##while True:
+##    if i%3 == 0:
+##        break
+##    print(i)
+##    i += 2
+
+##i = 1
+##while False:
+##    if i%2 == 0:
+##        break
+##    print(i)
+##    i += 2
+
+##True = False
+##while True:
+##    print(True)
+##    break
+
