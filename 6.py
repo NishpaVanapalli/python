38cbc8eec73d4c3be65df715161a830ae533da78
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-12-02 18:59:55 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,33 @@
+##var=1
+##while var<10:
+##    print("#")
+##    var=var<<1
+
+
+
+##nums=[1,2,3]
+##vals=nums[-1:-2]
+##print(vals)
+##print(nums)
+
+
+
+##x=1
+##x=x==x
+##print(x)
+
+
+##lst=[i for i in range(-1,2)]
+##print(lst)
+
+
+##vals=[0,1,2]
+##vals.insert(0,1)
+##del vals[1]
+##print(vals)
+
+
+
+##vals=[0,1,2]
+##vals[0],vals[2]=vals[2],vals[0]
+##print(vals[2])
