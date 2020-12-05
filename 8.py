0a462b87d48dde72da1242d02fa4283844a9aff1
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-12-05 18:58:28 +0530

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,40 @@
+##l1 = [1,2]
+##for v in range(2):
+##    l1.insert(-1,l1[v])
+##print(l1)
+
+
+##aList = [5, 10, 15, 25]
+##print(aList[::-2])
+
+
+##nums = [1,2,3]
+##vals = nums
+##del vals[:]
+##print(vals)
+
+
+##sampleList = [10, 20, 30, 40, 50]
+##print(sampleList[-2])
+##print(sampleList[-4:-1])
+
+
+##aList = [4, 8, 12, 16]
+##aList[1:4] = [20, 24, 28]
+##print(aList)
+
+
+##L = [i for i in range(-1,-2)]
+##print(L)
+
+
+##tup = [1, 2, 4, 8]
+##tup = tup[-2:-1]
+##tup = tup[-1]
+##print(tup)
+
+
+
+
+
+
