c5980d2d5ad4caa8f9aa9385146c4d7a8893f7b3
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-12-06 15:28:08 +0530

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,41 @@
+##sampleList = [10, 20, 30, 40, 50]
+##sampleList.append(60)
+##print(sampleList)
+##
+##sampleList.append(60)
+##print(sampleList)
+
+
+##sampleList = [10, 20, 30, 40, 50]
+##sampleList.pop()
+##print(sampleList)
+##
+##sampleList.pop(2)
+##print(sampleList)
+
+
+##lst = [3, 1, -2]
+##print(lst[lst[-1]])
+
+
+##lst = [1,2,3,4]
+##print(lst[-3:-2])
+
+
+##vals = [0, 1, 2]
+##vals.insert(0,1)
+##del vals[1]
+##print(vals)
+
+
+##nums = [1,2,3]
+##vals = nums
+##del vals[1:2]
+##print(nums)
+##print(vals)
+
+
+##nums = [1,2,3]
+##vals = nums[-1:-2]
+##print(vals)
+##print(nums)
