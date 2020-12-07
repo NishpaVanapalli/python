4eef798920e1ffa455f146915403899f9b36ebf8
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-12-07 20:04:58 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,39 @@
+##a=[1,2,3]
+##b=a.append(4)
+##print(a)
+##print(b)
+
+
+##a=[13,56,17]
+##a.append([87])
+##a.extend([45,67])
+##print(a)
+
+
+##list1=[3,2,5,7,3,6]
+##list1.pop(3)
+##print(list1)
+
+
+##list1=[1,3,5,2,4,6,2]
+##list1.remove(2)
+##print(sum(list1))
+
+
+##myList = [1, 5, 5, 5, 5, 1]
+##max = myList[0]
+##indexOfMax = 0
+##for i in range(1, len(myList)):
+##    if myList[i] > max:
+##        max = myList[i]
+##        indexOfMax = i
+##print(indexOfMax)
+
+
+
+##myList = [1, 2, 3, 4, 5, 6]
+##for i in range(1, 6):
+##    myList[i - 1] = myList[i]
+## 
+##for i in range(0, 6): 
+##    print(myList[i], end = " ")
