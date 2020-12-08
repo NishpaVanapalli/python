39893f0c4622abfff0ba488f60df902daa9fb3e1
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-12-08 19:00:04 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,40 @@
+##l1 = [1,2,3]
+##l2 = [ ]
+##for v in l1:
+##    l2.insert(0,v)
+##print(l2)
+
+
+##l1 = [1,2,3]
+##for v in range(len(l1)):
+##    l1.insert(1,l1[v])
+##print(l1)
+
+
+##sampleList = [10, 20, 30, 40]
+##del sampleList[0:6]
+##print(sampleList)
+
+
+##aList = [10, 20, 30, 40, 50, 60, 70, 80]
+##print(aList[2:5])
+##print(aList[:4])
+##print(aList[3:])
+
+
+##L = [[0, 1, 2, 3] for i in range(2)]
+##print(L[2][0])
+
+
+##list1=[8,0,9,5]
+##print(list1[::-1])
+
+
+##list1=[6,23,3,2,0,9,8,75]
+##print(list1[1:8:2])
+
+
+##tup = [1, 2, 4, 8]
+##tup = tup[1:-1]
+##tup = tup[0]
+##print(tup)
