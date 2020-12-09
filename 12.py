10a1e4d7daecf0c9196e208270376eb6cc6f419f
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-12-09 18:59:24 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,61 @@
+##str1 = 'hello'
+##str2 = ','
+##str3 = 'world'
+##print(str1[-1:])
+
+
+##str1="helloworld"
+##print(str1[::-1])
+
+
+##s="abcd"
+##s="abcd"[2:]
+##print(s)
+
+
+##print('new' 'line')
+
+
+##str1 = "my isname isisis jameis isis bond";
+##sub = "is";
+##print(str1.count(sub, 4))
+
+
+##example = "snow world"
+##print(example[4:7])
+
+
+##example = "snow world"
+##example[3] = 's'
+##print (example)
+
+
+##example = "helle"
+##print(example.find("e"))
+
+
+##example = "helle"
+##print(example.rfind("e"))
+
+
+##example="helloworld"
+##print(example[::-1].startswith("d"))
+
+
+##str1="Aplication"
+##str2=str1.replace('a','A')
+##print(str2)
+
+
+##str1="Information"
+##print(str1[2:8])
+
+
+##def fun(n=2,out=3):
+##    return n*out
+##print(fun(3))
+
+
+##tup=(1, ) + (1, )
+##tup= tup+tup
+##print(len(tup))
