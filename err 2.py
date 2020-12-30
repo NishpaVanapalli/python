2f5c4356233c47a14a8e33493a329a096b846ca6
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-12-30 21:04:29 +0530

    Add files via upload

---
+++ err 2.py
@@ -0,0 +1,39 @@
+##a=10
+##try:
+##    print(a+b)
+##except:
+##    print('hi')
+
+
+##a=10
+##print(a/0)
+##print(a+b)
+
+
+##a=10
+##try:
+##    print(a)
+####    print(a+b)
+####    print(a/0)
+##except NameError:
+##    print('avoid naming')
+##except ZeroDivisionError:
+##    print('cannot divide with zero')
+##else:
+##    print('no error')
+
+
+##a=10
+##try:
+##    print(a)
+####    print(a+b)
+##except NameError:
+##    print('avoid naming')
+##except ZeroDivisionError:
+##    print('cannot divide with Zero')
+##else:
+##    print('no error')
+##finally:
+##    print('done')
+
+
