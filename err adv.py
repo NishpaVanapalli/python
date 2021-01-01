64a98fb781cdfa54665143369487f941a667c514
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-01-01 12:35:55 +0530

    Add files via upload

---
+++ err adv.py
@@ -0,0 +1,44 @@
+##import math
+##import math as M
+##from math import *
+##print(M.sqrt(4))
+
+
+##import csv
+##with open("data.csv") as file 1:
+##    records=csv.reader(file 1)
+##    for record inrecords:
+##        print(record)
+
+
+
+##import csv
+##data=[]
+##with open("data.csv") as file 1:
+##    records=list(csv.reader(file 1))
+##    for record in records:
+##        dara.append(record)
+##print(len(data))
+
+
+##for row in data:
+##    print(row[5])
+
+
+##branches=[]
+##for row in data:
+##    branches.append(row[-2])
+##branches=list(set(branches))
+##print(len(branches))
+##for b in branches:
+##    print(b)
+##    
+
+
+##branches=[]
+##for row in data:
+##    branches.append(row[-2])
+##branches=list(set(branches))
+##for r in b:
+##    print(r,'-->',branches.count(r))
+    
