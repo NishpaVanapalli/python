af835d662fbae672c4d343d41c66b584ef4c36d7
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-01-08 20:00:52 +0530

    Add files via upload

---
+++ err hand.py
@@ -0,0 +1,61 @@
+##a=open('sample.txt','x')
+##a.close()
+
+
+##a=open('sample123.docx','x')
+##a.close()
+
+
+##a=open(r'example.txt','w')
+##a.write('hi,this is file handling class')
+##a.close()
+
+
+##a=open(r'example.txt','w')
+##a.write('hi,python session for degree students\n')
+##a.write('writing in second line')
+##a.close()
+
+
+
+##a=open(r'example.txt','a')
+##a.write('will be executed')
+##a.close()
+
+
+##a=open(r'example.txt','r')
+##b=a.read()
+##a.close
+##print(b)
+
+
+##import os
+##os.rmdir(r'example.txt')
+
+
+##a=open(r'example.txt','r')
+##b=a.read()
+##print(b)
+
+
+##import csv
+##a=open(r'cars.csv','r')
+##b=a.read()
+##print(b)
+
+
+
+##import csv
+##a=open(r'cars.csv','r')
+##b=a.readlines()
+##for i in b:
+##    print(i)
+##print(b)
+
+
+
+##a=open(r'example.txt','r')
+##b=a.read()
+##print(b)
+
+
