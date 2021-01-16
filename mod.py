89e42cef914731492055281392e12f7a653f85d7
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-01-16 18:35:37 +0530

    Add files via upload

---
+++ mod.py
@@ -0,0 +1,50 @@
+##import random
+##for i in range(5):
+##    a=random.random()
+##    print(a)
+
+
+##import sys
+##print(sys.path)
+
+
+##import random
+##for i in range(5):
+##    a=random.uniform(1,3)
+##    print(a)
+    
+
+
+##import random
+##for i in range(5):
+##    a=random.randint(1,5)
+##    print(a)
+
+
+##import random
+##for i in range(5):
+##    a=random.randrange(1,5)
+##    print(a)
+
+
+##import random
+##for i in range(5):
+##    a=random.randrange(2,10,2)
+##    print(a)
+
+
+
+##import random
+##a=['sachin','kohli','rohit']
+##for i in range(5):
+##    print(random.choice(a))
+##    print(a)
+
+
+##a=['sachin','kohli','rohit','dhoni']
+##print(a)
+##for i in range(3):
+##    random.shuffle(a)
+##    print(a)
+
+
