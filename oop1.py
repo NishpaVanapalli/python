cfe5145ed3947027a99f006d7f5f25ea48254d6c
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-05-22 10:50:58 +0530

    Add files via upload

---
+++ oop1.py
@@ -0,0 +1,11 @@
+##class Human:
+##    pass
+##hb1-HB("Sudhir")
+##hb2=HB("Sreenu")
+
+
+class Student:
+    def __init__(self,name,rollno):
+        self.name=name
+        self.rollno=rollno
+std1=Student("Siva",123)
