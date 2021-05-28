ca699d989697d9ee0dea9e341abd03143850b473
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-05-28 10:30:12 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,58 @@
+##print('my first python program')
+##print(2+3)
+################################
+
+
+##print("This program calculates the average of two numbers. ")
+##print("The numbers are 4 and 8")
+##print("The average is: ", (4+8)/2)
+######################################
+
+
+##print("This program calculates the average of two numbers. ")
+##number1=10
+##number2=19
+##print("The numbers are", number1, "and", number2)
+##print("The average is: ", (number1+number2)/2)
+################################################
+
+
+##print("This program calculates the average of two numbers. ")
+##
+##number1 =float( input("Please enter the first number: ") )
+##number2 = float( input("Please enter the second number: ") )
+##
+##print("The numbers are", number1, "and", number2)
+##print("The average is: ", (number1+number2)/2)
+##################################################
+
+
+##km = float( input("Type the number of kilometers: ") )
+##miles= km / 1.609344
+##print(km, "km=", round(miles,4), "miles")
+########################################################
+
+
+##first_name  =  input("What's your first name? ")
+##middle_name  =  input("What's your middle name? ")
+##last_name  =  input("What's your last name? ")
+##print("Your initials are", first_name[0], middle_name[0], last_name[0])
+##########################################################
+
+
+##lot_number = "037-00901-00027"
+##print("Country code: ", lot_number[:3])
+##print("Product code: ", lot_number[4:9])
+##print("Batch number: ", lot_number[10:])
+############################################################
+
+
+##import math
+##radius= float(input("Enter the radius of the number: "))
+##area = math.pi *(radius**2)
+##circumference=2*math.pi*radius
+##print("The area of the circle is: ", round(area,2))
+##print("The circumference of the circle is: ", round(circumference,2))
+############################################################
+
+
