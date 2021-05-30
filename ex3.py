6547e192dccc43e13c787d684117ff27f37c9073
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-05-30 09:46:30 +0530

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,62 @@
+##x = 0
+##people = []
+##while x < 5:
+##    person = input("Type the name of a person: ")
+##    people.append(person)
+##    x += 1
+##print(people)
+################################################################
+
+
+##people = []
+##while len(people) < 5:
+##    person = input("Type the name of a person: ")
+##    people.append(person)
+##print(people)
+##################################################################
+
+
+##import random
+##number = random.randint(0,10)
+##guess = int(input("I'm thinking about a number between zero and ten. Can you guess it? "))
+##while True:
+##    if guess==number:
+##        break
+##    else:
+##        guess = int(input("Nope. Try again: "))
+##print("You guessed it. I was thinking about ", number)
+############################################################################
+
+
+##blog_posts = [" ", "The 10 coolest math functions in Python", " ", "How to make HTTP requests in Python", "A tutorial about data types in Python"]
+##for post in blog_posts:
+##    if post == " ":
+##        continue
+##    else:
+##        print(post)
+##
+##print("------------------------------")
+##mystring = "This is a string"
+##for char in mystring:
+##    print(char)
+##
+##
+##print("------------------------------")
+##for x in range(0,10):
+##    print(x)
+##
+##print("------------------------------")
+##person = {"Name" : "Karen Smith", "Age" : 25, "Gender" : "Female"}
+##for key in person:
+##    print(key, ":" , person[key])
+##
+##
+##print("------------------------------")
+##blog_posts =  {"Python" : ["The 10 coolest math functions in Python", "How to make HTTP requests in Python", "A tutorial about data types in Python"], "Javascript" : ["Namespaces in Javascript", "New functions available in ES6"] }
+##for category in blog_posts:
+##    print("Posts about ", category)
+##    for post in blog_posts[category]:
+##        print(post)
+####################################################################
+
+
