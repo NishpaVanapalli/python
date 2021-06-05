668caf9eadcb7fab68c74a48bb2d7c635e10fbe6
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-06-05 08:51:44 +0530

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,13 @@
+##f = open("test2.txt", "x")
+
+
+##############################################
+##f = open("test.txt", "a")
+##f.write("\n This text will be appended to the file")
+##
+##f = open("test.txt")
+##print(f.read())
+
+######################################################
+
+
