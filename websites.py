a832e61aafb745513ced871293e46a52ee452ca7
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-06-22 12:27:39 +0530

    Add files via upload

---
+++ websites.py
@@ -0,0 +1,33 @@
+VARIOUS MODULES IN PYTHON
+
+https://docs.python-requests.org/en/master/   --------------->  do http requests in python
+https://www.wxpython.org/    ----------------------------->  used to create graphic interface for python programs
+https://www.sqlalchemy.org/   ----------------------------->  work with sql databases
+https://scipy.org/       ------------------------------------>  library of algorithms and mathematical tools for scientific calculations
+https://www.pygame.org/news   ---------------------------->  create games in python
+https://scikit-learn.org/stable/     -------------------------->  machine learning in python
+kaggle.com  --------------------------------------------->  large community for machine learning
+
+
+##################################################
+
+HTTP Status Codes
+
+200  ----->  OK
+403  ----->  Forbidden
+404  ----->  Not Found
+
+
+######################################################################
+
+
+API  ------->  Application Programming Interface
+JSON  ------>  JavaScript Object Notation
+KNN  ------->  K nearest neighbours(module)
+
+##############################################################################
+
+
+Logistic Regression  ---------------->  used in machine learning categoric predictions
+                                                        normally used in binary classifications
+                                                        
